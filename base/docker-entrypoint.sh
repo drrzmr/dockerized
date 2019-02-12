a29f8c5285b89fc4bf8603f06e03e5f417cdb13d
Eder Ruiz <drrzmr@gmail.com>
2019-02-12 16:13:50 -0200

    Add parameter to --exit-on-error to run-parts

--- base/docker-entrypoint.sh
+++ base/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
 DOCKER_ENTRYPOINT_DIR="/docker-entrypoint.d"
 
-[[ -d ${DOCKER_ENTRYPOINT_DIR} ]] && /bin/run-parts --verbose ${DOCKER_ENTRYPOINT_DIR}
+[[ -d ${DOCKER_ENTRYPOINT_DIR} ]] && /bin/run-parts --exit-on-error --verbose ${DOCKER_ENTRYPOINT_DIR}
 
+set +e
 exec ${@}
